cb2384bae264f4fadb4efa252474fb3ca71f76dd
R <rockleeserver3@gmail.com>
2022-02-14 14:55:59 +0900

    Deleteされない

--- app/src/main/java/com/example/roomdbtest/MainActivity.java
+++ app/src/main/java/com/example/roomdbtest/MainActivity.java
@@ -27,28 +27,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.index);
+        Button delete_btn = findViewById(R.id.delete_button);
         Button btn = findViewById(R.id.button);
         AppDatabase database = AppDatabaseSingleton.getInstace(getApplicationContext());
 
-        btn.setOnClickListener(new ButtonClickListenr(this,database,textView));
+        btn.setOnClickListener(new ButtonClickListenr(this,database,textView,true));
+        delete_btn.setOnClickListener(new ButtonClickListenr(this,database,textView,false));
 
+        Log.d(debugTag,"onCreate");
     }
 
     private class ButtonClickListenr implements View.OnClickListener{
         private Activity activity;
         private AppDatabase database;
         private TextView textView;
+        private boolean btn_flag;
 
-        public ButtonClickListenr(Activity activity, AppDatabase database, TextView tv) {
+        public ButtonClickListenr(Activity activity, AppDatabase database, TextView tv,boolean btn_flag) {
             this.activity = activity;
             this.database = database;
             this.textView = tv;
+            this.btn_flag = btn_flag;
         }
 
         @Override
         public void onClick(View v) {
-            Log.d("debugTag","onClick");
-            new DataStoreAsyncTask(database, activity, textView).execute();
+            Log.d("MainActivity","onClick");
+            new DataStoreAsyncTask(database, activity, textView,btn_flag).execute();
         }
     }
 
@@ -57,24 +62,37 @@ public class MainActivity extends AppCompatActivity {
         private AppDatabase database;
         private TextView textView;
         private StringBuilder stringBuilder;
+        private boolean btn_flag;
 
-        public DataStoreAsyncTask(AppDatabase database,Activity activity,TextView textView){
+        public DataStoreAsyncTask(AppDatabase database,Activity activity,TextView textView,boolean btn_flag){
             this.database = database;
             weakActivity = new WeakReference<>(activity);
             this.textView = textView;
+            this.btn_flag = btn_flag;
         }
 
         @Override
         protected Integer doInBackground(Void...params){
+            Log.d("MainActivity",new Timestamp(System.currentTimeMillis()).toString());
             AccessTimeDao accessTimeDao = (AccessTimeDao) database.accessTimeDao();
-            Log.d("new AccessTime",new Timestamp(System.currentTimeMillis()).toString());
-            accessTimeDao.insert(new AccessTime(new Timestamp(System.currentTimeMillis()).toString()));
+
+            if(btn_flag){
+                Log.d("MainActivity","add");
+                accessTimeDao.insert(new AccessTime(new Timestamp(System.currentTimeMillis()).toString()));
+            }else {
+                Log.d("MainActivity","delete");
+                AccessTime accessTime = new AccessTime("");
+                accessTime.setId(0);
+                accessTimeDao.delete(accessTime);
+            }
+
 
             stringBuilder = new StringBuilder();
             List<AccessTime> atList = accessTimeDao.getAll();
             for (AccessTime at: atList) {
                 stringBuilder.append(at.getAccessTime()).append("\n");
             }
+            Log.d("MainActivity","");
 
             return 0;
         }
